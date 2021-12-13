503546abf2bf18aaa1222a71f7f247a32acdbdad
micheljacop <micheljacop@hotmail.com>
2021-12-13 19:15:27 +1000

    * Employee.cs: Adding PrintRecords method
    * Program.cs: sorting employees list, selecting and printing first 3
      records

--- csharp_cli_tool/Employee.cs
+++ csharp_cli_tool/Employee.cs
@@ -5,11 +5,18 @@ namespace csharp_cli_tool
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Date { get; set; }
+        public DateTime Date { get; set; }
         public Int16 Division { get; set; }
         public Int16 Points { get; set; }
         public string Summary { get; set; }
 
-  
+        // printing YAML formatted description of employee
+        public string PrintRecords()
+        {
+            return ($"- name: {FirstName} {LastName} \n  details: In division {Division} from {Date.ToString("yyyy-MM-dd")} performing {Summary}");
+
+        }
+
+
     }
 }
--- csharp_cli_tool/Program.cs
+++ csharp_cli_tool/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using YamlDotNet.Serialization.NamingConventions;
+using YamlDotNet.Serialization;
 
 namespace csharp_cli_tool
 {
@@ -26,7 +29,7 @@ namespace csharp_cli_tool
                 Employee employee = new Employee();
                 employee.FirstName = records[0];
                 employee.LastName = records[1];
-                employee.Date = records[2];
+                employee.Date = Convert.ToDateTime(records[2]);
                 employee.Division = Convert.ToInt16(records[3]);
                 employee.Points = Convert.ToInt16(records[4]);
                 employee.Summary = records[5];
@@ -35,8 +38,24 @@ namespace csharp_cli_tool
 
 
             }
+
+            // ordering the list by division then by points
+            employees = employees.OrderBy(emp => emp.Division).ThenByDescending(emp => emp.Points).ToList();
+
+            // selecting the top 3 records from the sorted list
+            List<Employee> selectedEmployees = new List<Employee>();
+            selectedEmployees = employees.Take(3).ToList();
+
+            // Printing the selected employees to the console
            
-           
+            Console.WriteLine("records:");
+            foreach (var employee in selectedEmployees)
+            {
+                Console.WriteLine(employee.PrintRecords());
+
+
+            }
+
 
             // Stopping the console program from closing 
             Console.ReadLine();
