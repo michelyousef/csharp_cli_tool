64e0e8ff4e02ee8120ccf7efba182ecfa08e0112
micheljacop <micheljacop@hotmail.com>
2021-12-13 13:17:37 +1000

    * Employee.cs: creating Employee class to store records with the relevant data
    * records.csv: Adding a local csv file with few records inside the
      project folder
    
    * Program.cs: reading csv file and storing all records in the
      employees list, so it can be sorted and extract relevant data from
      it

---
+++ csharp_cli_tool/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace csharp_cli_tool
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Date { get; set; }
+        public Int16 Division { get; set; }
+        public Int16 Points { get; set; }
+        public string Summary { get; set; }
+
+  
+    }
+}
--- csharp_cli_tool/Program.cs
+++ csharp_cli_tool/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace csharp_cli_tool
 {
@@ -6,7 +8,38 @@ namespace csharp_cli_tool
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello CLI tool!");
+
+
+            // creating a list of all records
+            List<Employee> employees = new List<Employee>();
+            string fileName = "records.csv";
+            // retrieving relative file path in the project
+            string fullFilePath = Path.Combine("../../..", fileName);
+            // reading file data and storing it in a string array
+            string[] myLines = File.ReadAllLines(fullFilePath);
+            
+            foreach (var line in myLines)
+            {
+                // storing each line in the csv file as an array
+                string[] records = line.Split(',');
+                // adding each line in a separate object in the Employee class
+                Employee employee = new Employee();
+                employee.FirstName = records[0];
+                employee.LastName = records[1];
+                employee.Date = records[2];
+                employee.Division = Convert.ToInt16(records[3]);
+                employee.Points = Convert.ToInt16(records[4]);
+                employee.Summary = records[5];
+                // adding the record to the employees list 
+                employees.Add(employee);
+
+
+            }
+           
+           
+
+            // Stopping the console program from closing 
+            Console.ReadLine();
         }
     }
 }
