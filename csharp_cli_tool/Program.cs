b18c098d1997e96d248ef6051d1b3947f3110501
micheljacop <micheljacop@hotmail.com>
2021-12-14 11:01:07 +1000

    Refactoring AddingEmployees in a separate method Adding error checking

--- csharp_cli_tool/Program.cs
+++ csharp_cli_tool/Program.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using YamlDotNet.Serialization.NamingConventions;
-using YamlDotNet.Serialization;
+
 
 namespace csharp_cli_tool
 {
@@ -19,12 +18,55 @@ namespace csharp_cli_tool
             // retrieving relative file path in the project
             string fullFilePath = Path.Combine("../../..", fileName);
             // reading file data and storing it in a string array
-            string[] myLines = File.ReadAllLines(fullFilePath);
-            
-            foreach (var line in myLines)
+            // checking if the file path is correct
+            try
             {
-                // storing each line in the csv file as an array
-                string[] records = line.Split(',');
+                string[] myLines = File.ReadAllLines(fullFilePath);
+                employees = AddingEmployees(myLines);
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+
+
+            // ordering the list by division then by points
+            employees = employees.OrderBy(emp => emp.Division).ThenByDescending(emp => emp.Points).ToList();
+
+            // selecting the top 3 records from the sorted list
+            List<Employee> selectedEmployees = new List<Employee>();
+            // making sure employees list has at least 3 records
+            if (employees.Count >= 3)
+            selectedEmployees = employees.Take(3).ToList();
+
+            // Printing the selected employees to the console
+           
+            Console.WriteLine("records:");
+            foreach (var employee in selectedEmployees)
+            {
+                Console.WriteLine(employee.PrintRecords());
+
+            }
+
+
+            // Stopping the console program from closing 
+            Console.ReadLine();
+        }
+
+
+        // Adding records to employees list
+        public static List<Employee> AddingEmployees(string[] fileData)
+        {
+            List<Employee> employees = new List<Employee>();
+
+
+            foreach (var entry in fileData)
+            {
+                // splitting each line into an array
+                string[] records = entry.Split(',');
                 // adding each line in a separate object in the Employee class
                 Employee employee = new Employee();
                 employee.FirstName = records[0];
@@ -39,26 +81,7 @@ namespace csharp_cli_tool
 
             }
 
-            // ordering the list by division then by points
-            employees = employees.OrderBy(emp => emp.Division).ThenByDescending(emp => emp.Points).ToList();
-
-            // selecting the top 3 records from the sorted list
-            List<Employee> selectedEmployees = new List<Employee>();
-            selectedEmployees = employees.Take(3).ToList();
-
-            // Printing the selected employees to the console
-           
-            Console.WriteLine("records:");
-            foreach (var employee in selectedEmployees)
-            {
-                Console.WriteLine(employee.PrintRecords());
-
-
-            }
-
-
-            // Stopping the console program from closing 
-            Console.ReadLine();
+            return employees;
         }
     }
 }
