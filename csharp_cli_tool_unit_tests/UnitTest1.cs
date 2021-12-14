f084503f3d63dc0503ed44028992f6ea56457422
micheljacop <micheljacop@hotmail.com>
2021-12-14 12:10:10 +1000

    Unit tests class

---
+++ csharp_cli_tool_unit_tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using csharp_cli_tool;
+
+namespace csharp_cli_tool_unit_tests
+{
+    public class ProgramTests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void AddingEmployeesCase()
+        {
+            //Arrange
+            Employee employee = new Employee();
+            employee.FirstName = "M";
+            employee.LastName = "Y";
+            employee.Date = new System.DateTime();
+            employee.Division = 4;
+            employee.Points = 34;
+            employee.Summary = "Offensive Duties";
+
+            //Act
+
+            //Assert
+            Assert.Pass();
+        }
+    }
+}
