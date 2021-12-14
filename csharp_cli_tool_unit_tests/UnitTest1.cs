a81276b7127d0c3266a897892890d18b8b3fc989
micheljacop <micheljacop@hotmail.com>
2021-12-14 13:23:16 +1000

    basic unit test for Program class

---
+++ csharp_cli_tool_unit_tests/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using csharp_cli_tool;
+using System.IO;
+
+namespace csharp_cli_tool_unit_tests
+{
+    public class ProgramTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void AddingEmployeesTest()
+        {
+            //Arrange
+            string fileName = "records.csv";
+            // retrieving relative file path in the project
+            string fullFilePath = Path.Combine("../../..", fileName);
+            string[] myLines = File.ReadAllLines(fullFilePath);
+
+
+            //Act
+
+            //Assert
+            Assert.IsNotNull(myLines);
+        }
+    }
+}
